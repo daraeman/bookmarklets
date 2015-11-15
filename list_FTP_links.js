1007a04ba96b2a0ef1f55b79bdcc2642c81486dc
DR <daraeman@gmail.com>
2015-11-15 01:04:39 -0800

    Update list_FTP_links.js

--- list_FTP_links.js
+++ list_FTP_links.js
@@ -1,4 +1,9 @@
 
+/*
+	Creates a textarea at the top of the page with
+	each link that isn't a folder on a separate line
+*/
+
 // unminified
 javascript:(function(){
 	var links = document.body.getElementsByTagName( "table" )[0].getElementsByTagName( "a" );
@@ -18,3 +23,4 @@ javascript:(function(){
 
 // minified
 javascript:(function(){var e=document.body.getElementsByTagName("table")[0].getElementsByTagName("a"),t=document.createElement("textarea"),n="";for(i=0;i<e.length;i++)"/"!=e[i].href[e[i].href.length-1]&&(n+=e[i].href+"\n");t.value=n,t.style.width="100%",t.style.minHeight="100px",t.style.marginBottom="10px",document.body.insertBefore(t,document.body.getElementsByTagName("p")[0])})();
+
